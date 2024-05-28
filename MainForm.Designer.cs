c17f1b721f4326703aa823fa10665f097d11bff3
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-05-28 04:01:55 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 5/28/2024
+ * Time: 2:55 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace openAndCloseRectangle
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(707, 90);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Red;
+			this.panel1.Location = new System.Drawing.Point(687, 114);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(16, 16);
+			this.panel1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(788, 90);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1282, 599);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "openAndCloseRectangle";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
